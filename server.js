8f1cb1180996c09fe7a9ac8664535e0d5b248250
Nazi <nazanin1427@gmail.com>
2014-04-27 22:24:18 -0500

    produce html

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
-var http = require('http');
-http.createServer(function(request, response){
-	response.writeHead(200,{"Content-Type" : " text/plain"});
-	response.write("Hello World!");
+var url = require("url");
+var http = require("http");
+var fs = require('fs');
+
+function read(response){
+	fs.readFile("page.html","utf8",function read(err, data){
+	if(err){
+		response.end();
+		return;
+	}
+	response.write(data);	
 	response.end();
-}).listen(8080);
+	
+		});
+}
+function start(){
+	function onRequest(request, response) {
+		console.log(url.parse(request.url, true));
+		response.writeHead(200, {"Content-Type": "text/html"});
+		read(response);
+	}
+	
+http.createServer(onRequest).listen(8080);
+console.log("Server has started.");
+}
+
+
+exports.start = start;
